66e99a17e217afcefa2bf6605ea475aeed23a748
darkfoxdeveloper <46323771+darkfoxdeveloper@users.noreply.github.com>
2019-01-02 17:16:01 +0100

    Flarum Spanish Language pack
    Works in 0.1.0 Beta 8

---
+++ bootstrap.php
@@ -0,0 +1,77 @@
+<?php
+/*
+ * This file is part of DaRkFoxDeveloper/FlarumES.
+ * -----------------------------------------------------------------------
+ * Copyright © 2015-2018 Toby Zerner and Flarum
+ * Copyright © 2015-2018 DaRkFoxDeveloper and Flarum en Español
+ * -----------------------------------------------------------------------
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Flarum\Event\ConfigureClientView;
+use Illuminate\Contracts\Events\Dispatcher;
+use Flarum\Settings\SettingsRepositoryInterface;
+use Flarum\Event\ConfigureLocales;
+
+return function (Dispatcher $events) {
+    $events->subscribe('Spanish');
+};
+
+class Spanish {
+    protected $settings;
+
+    public function __construct(SettingsRepositoryInterface $settings) {
+        $this->settings = $settings;
+    }
+
+    public function subscribe(Dispatcher $events) {
+        $events->listen(ConfigureClientView::class, [$this, 'addSettingsPage']);
+        $events->listen(ConfigureLocales::class, [$this, 'addLocale']);
+    }
+
+    public function addSettingsPage(ConfigureClientView $event) {
+        if ($event->isAdmin()) {
+            $event->addAssets([
+                __DIR__ . '/js/admin/dist/extension.js'
+            ]);
+            $event->addBootstrapper('DaRkFoxDeveloper/FlarumES/main');
+        }
+    }
+
+    public function addLocale(ConfigureLocales $event) {
+        $name = $title = basename(__DIR__);
+
+        if (file_exists($manifest = __DIR__.'/composer.json')) {
+            $json = json_decode(file_get_contents($manifest), true);
+
+            if (empty($json)) {
+                throw new RuntimeException("Error parsing composer.json in $name: ".json_last_error_msg());
+            }
+
+            $locale = array_get($json, 'extra.flarum-locale.code');
+            $title = array_get($json, 'extra.flarum-locale.title', $title);
+        }
+
+        if (! isset($locale)) {
+            throw new RuntimeException("Language pack $name must define \"extra.flarum-locale.code\" in composer.json.");
+        }
+
+        $event->locales->addLocale($locale, $title);
+        $mode = $this->settings->get('flarumes-spanish.mode') ? $this->settings->get('flarumes-spanish.mode') : 'formal';
+
+        if (! is_dir($localeDir = __DIR__.'/locale/' . $mode)) {
+            throw new RuntimeException("Language pack $name must have a \"locale\" subdirectory.");
+        }
+
+        if (file_exists($file = $localeDir.'/config.js')) {
+            $event->locales->addJsFile($locale, $file);
+        }
+
+        foreach (new DirectoryIterator($localeDir) as $file) {
+            if ($file->isFile() && in_array($file->getExtension(), ['yml', 'yaml'])) {
+                $event->locales->addTranslations($locale, $file->getPathname());
+            }
+        }
+    }
+}
---
+++ js/admin/src/main.js
@@ -0,0 +1,18 @@
+/*
+ * This file is part of DaRkFoxDeveloper/FlarumES.
+ * -----------------------------------------------------------------------
+ * Copyright © 2015-2018 Toby Zerner and Flarum
+ * Copyright © 2015-2018 Johann Rodríguez and Flarum en Español
+ * -----------------------------------------------------------------------
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import { extend } from 'flarum/extend';
+import app from 'flarum/app';
+
+import SpanishSettingsModal from 'DaRkFoxDeveloper/FlarumES/components/SpanishSettingsModal';
+
+app.initializers.add('flarumes-spanish', app => {
+  app.extensionSettings['flarumes-spanish'] = () => app.modal.show(new SpanishSettingsModal());
+});
