de32a1b34184361170420742000ea90025a3cf83
darkfoxdeveloper <46323771+darkfoxdeveloper@users.noreply.github.com>
2019-01-02 17:18:49 +0100

    Updated Copyright File Headers

--- bootstrap.php
+++ bootstrap.php
@@ -2,8 +2,8 @@
 /*
  * This file is part of DaRkFoxDeveloper/FlarumES.
  * -----------------------------------------------------------------------
- * Copyright © 2015-2018 Toby Zerner and Flarum
- * Copyright © 2015-2018 DaRkFoxDeveloper and Flarum en Español
+ * Copyright © 2015-2019 Toby Zerner and Flarum
+ * Copyright © 2015-2019 DaRkFoxDeveloper and Flarum en Español
  * -----------------------------------------------------------------------
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- js/admin/src/main.js
+++ js/admin/src/main.js
@@ -1,8 +1,8 @@
 /*
  * This file is part of DaRkFoxDeveloper/FlarumES.
  * -----------------------------------------------------------------------
- * Copyright © 2015-2018 Toby Zerner and Flarum
- * Copyright © 2015-2018 Johann Rodríguez and Flarum en Español
+ * Copyright © 2015-2019 Toby Zerner and Flarum
+ * Copyright © 2015-2019 Johann Rodríguez and Flarum en Español
  * -----------------------------------------------------------------------
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
