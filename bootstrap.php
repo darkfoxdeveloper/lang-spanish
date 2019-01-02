ec70a9be46fa7f75edac0976f8337125738b91ca
darkfoxdeveloper <46323771+darkfoxdeveloper@users.noreply.github.com>
2019-01-02 19:34:24 +0100

    Some changes

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,7 @@
  * This file is part of DaRkFoxDeveloper/FlarumES.
  * -----------------------------------------------------------------------
  * Copyright © 2015-2019 Toby Zerner and Flarum
- * Copyright © 2015-2019 DaRkFoxDeveloper and Flarum en Español
+ * Copyright © 2015-2019 DaRkFoxDeveloper
  * -----------------------------------------------------------------------
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
